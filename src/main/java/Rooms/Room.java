bfe7955a3d993dbe442e3af98844fa20f23a0186
Stanhoucke <stanhoucke@gmail.com>
2021-02-11 11:47:28 +0000

    passed guest tests

--- src/main/java/Rooms/Room.java
+++ src/main/java/Rooms/Room.java
@@ -1,26 +1,28 @@
-package Rooms;
-
-public abstract class Room {
-
-    private int capacity;
-    private RoomType roomType;
-    private ArrayList<Guest> collectionOfGuests;
-
-    public Room(int capacity, RoomType roomType, ArrayList<Guest> collectionOfGuests){
-        this.capacity = capacity;
-        this.roomType = roomType;
-        this.collectionOfGuests = collectionOfGuests;
-    }
-
-    public int getCapacity() {
-        return this.capacity;
-    }
-
-    public RoomType getRoomType() {
-        return this.roomType;
-    }
-
-    public ArrayList<Guest> getCollectionOfGuests() {
-        return this.collectionOfGuests;
-    }
-}
+//package Rooms;
+//
+//import java.util.ArrayList;
+//
+//public abstract class Room {
+//
+//    private int capacity;
+//    private RoomType roomType;
+//    private ArrayList<Guest> collectionOfGuests;
+//
+//    public Room(int capacity, RoomType roomType, ArrayList<Guest> collectionOfGuests){
+//        this.capacity = capacity;
+//        this.roomType = roomType;
+//        this.collectionOfGuests = collectionOfGuests;
+//    }
+//
+//    public int getCapacity() {
+//        return this.capacity;
+//    }
+//
+//    public RoomType getRoomType() {
+//        return this.roomType;
+//    }
+//
+//    public ArrayList<Guest> getCollectionOfGuests() {
+//        return this.collectionOfGuests;
+//    }
+//}
