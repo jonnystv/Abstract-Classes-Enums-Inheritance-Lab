9e464cb9cd7e875f481ebb22147809e141c1969b
Stanhoucke <stanhoucke@gmail.com>
2021-02-11 11:54:07 +0000

    enum RoomType

--- src/main/java/Rooms/RoomType.java
+++ src/main/java/Rooms/RoomType.java
@@ -1,4 +1,20 @@
 package Rooms;
 
 public enum RoomType {
+
+    SINGLE(1),
+    DOUBLE(2),
+    FAMILY(4),
+    MEETING(12),
+    FUNCTION(100);
+
+    private final int roomCapacity;
+
+    RoomType(int roomCapacity){
+        this.roomCapacity = roomCapacity;
+    }
+
+    public int getRoomCapacity(){
+        return this.roomCapacity;
+    }
 }
