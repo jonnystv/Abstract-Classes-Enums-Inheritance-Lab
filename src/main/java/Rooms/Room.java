cd8064e56041e00a0d8c81bfdfebba1017166ec7
Stanhoucke <stanhoucke@gmail.com>
2021-02-11 11:31:02 +0000

    initial room class setup

--- src/main/java/Rooms/Room.java
+++ src/main/java/Rooms/Room.java
@@ -1,4 +1,26 @@
 package Rooms;
 
-public class Room {
+public abstract class Room {
+
+    private int capacity;
+    private RoomType roomType;
+    private ArrayList<Guest> collectionOfGuests;
+
+    public Room(int capacity, RoomType roomType, ArrayList<Guest> collectionOfGuests){
+        this.capacity = capacity;
+        this.roomType = roomType;
+        this.collectionOfGuests = collectionOfGuests;
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public RoomType getRoomType() {
+        return this.roomType;
+    }
+
+    public ArrayList<Guest> getCollectionOfGuests() {
+        return this.collectionOfGuests;
+    }
 }
